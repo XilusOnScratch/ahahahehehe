e45f1d47c34cc4cbb88a60365d5f12b831a3dedf
naman-sonawane <sonawane.naman14@gmail.com>
2026-02-16 16:06:52 -0500

    clean

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyBl20VCma3oP8I1N8Uug5hOhq0mTFpgE4M")
-response = client.models.generate_content(
-    model="gemini-3-flash-preview",
-    contents="Explain how AI works in a few words"
-)
-print(response.text)
